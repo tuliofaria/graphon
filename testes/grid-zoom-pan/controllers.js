e8e3fd75f59a2ad062b1961d33b307ee484b4eac
Romenig Ribeiro <romenig@gmail.com>
2013-10-08 20:18:54 -0300

    Atualizando...

--- testes/grid-zoom-pan/controllers.js
+++ testes/grid-zoom-pan/controllers.js
@@ -1,32 +1,33 @@
 function MainCtrl($scope){
 	$scope.parameters= {
 	}
-        $scope.lastDrawedExp = "";
+  
+  $scope.lastDrawedExp = "";
 
 	$scope.updateParams = function(){
         	var latex = $("#bla").mathquill('latex');
-                var calc = new Calc(latex);
-                var ps = calc.params();
-                angular.forEach(ps, function(value, key){
-                	if(value!="x"){
-                        	if(!(value in $scope.parameters)){
-                        		$scope.parameters[value] = {
-                        			name: value,
-                        			starting: 0,
-                        			ending: 50,
-                                                currentValue: 25
-                        		};
-                        	}
-                        }
-                });
-                angular.forEach($scope.parameters, function(value, key){
-                        if(ps.indexOf(value.name)<0){
-                                delete $scope.parameters[key];
-                        }
-                });
-                $scope.$apply();
-                //$scope.parameters = calc.params();
-                $scope.drawFunction();
+          var calc = new Calc(latex);
+          var ps = calc.params();
+          angular.forEach(ps, function(value, key){
+          	if(value!="x"){
+                  	if(!(value in $scope.parameters)){
+                  		$scope.parameters[value] = {
+                  			name: value,
+                  			starting: 0,
+                  			ending: 50,
+                                          currentValue: 25
+                  		};
+                  	}
+                  }
+          });
+          angular.forEach($scope.parameters, function(value, key){
+                  if(ps.indexOf(value.name)<0){
+                          delete $scope.parameters[key];
+                  }
+          });
+          $scope.$apply();
+          //$scope.parameters = calc.params();
+          $scope.drawFunction();
 	}
         $scope.drawFunction = function(){
           if(graficoLayer!=null){
